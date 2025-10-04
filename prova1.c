b52a537fb18aefce37415927b01329afaf3092b1
José Adalberto Façanha Gualeve <jose.gualeve.cigr@telebras.com.br>
2025-10-04 16:07:49 -0300

    Prova 1 - strings

--- prova1.c
+++ prova1.c
@@ -1,32 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
-#define N 15
+#define N 50
+int main()
+{
+    int hora, minuto;
+    printf("Digite uma hora (hh:mm) ");
+    scanf("%d:%d", &hora, &minuto);
+    if (hora < 12) {
+        printf("Bom dia\n");
+    } 
+    if (hora > 12 && hora < 18)
+        printf("Boa tarde\n");
+    if (hora >= 18)
+        printf("Boa noite\n");
 
-int main() {
-    int vetor[N];
-
-    for (int i=0; i<N; i++) {
-        vetor[i] = N - i;
-    }
-    for (int i=0; i<N; i++) {
-        printf("%d ", vetor[i]);
-    }
-    printf("\n");
-
-
-    // int hora, minuto;
-
-    // printf("Digite um horário (HH:MM) ");
-    // scanf("%d:%d", &hora, &minuto);
-
-    // if (hora < 12) {
-    //     printf("Bom dia\n");
+    // int vetor[N];
+    // for (int i=0; i<N; i++) {
+    //     vetor[i] = i * 2;
     // }
-    // else {
-    //     printf("Boa %s\n", (hora < 16) ? "tarde" : "noite");
-    //     // if (hora < 16)
-    //     //     printf("Boa tarde\n");
-    //     // else
-    //     //     printf("Boa noite\n");
+    // for (int i=0; i<N; i++) {
+    //     printf("%d ", vetor[i]);
     // }
+    
+    
+    
+    
+    
     return 0;
 }
